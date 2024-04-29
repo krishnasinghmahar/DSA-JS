2e938d92a43227b74239ab32d7079f61bb918b9f
Krishna Singh Mahar <krishnasinghmaharksm@gmail.com>
2024-04-29 17:06:05 +0530

    create: mergeSort

---
+++ recurssion/mergeSort.js
@@ -0,0 +1,47 @@
+function mergeSort(array) {
+  if (array.length <= 1) {
+    return array;
+  }
+
+  const middle = Math.floor(array.length / 2);
+  const left = array.slice(0, middle);
+  const right = array.slice(middle);
+
+  const sortedLeft = mergeSort(left);
+  const sortedRight = mergeSort(right);
+
+  return merge(sortedLeft, sortedRight);
+}
+
+function merge(left, right) {
+  let result = [];
+  let leftIndex = 0;
+  let rightIndex = 0;
+
+  while (leftIndex < left.length && rightIndex < right.length) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  while (leftIndex < left.length) {
+    result.push(left[leftIndex]);
+    leftIndex++;
+  }
+
+  while (rightIndex < right.length) {
+    result.push(right[rightIndex]);
+    rightIndex++;
+  }
+
+  return result;
+}
+
+const array = [3, 2, 1, 13, 8, 5, 0, 1];
+console.log("Original array:", array);
+const sortedArray = mergeSort(array);
+console.log("Sorted array:", sortedArray);
